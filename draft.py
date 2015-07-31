4ef6284b1f67aeb84a71108cb2d62ceb0570d05a
Keith Ch <keith.chirayus@gmail.com>
2015-07-30 19:41:24 -0700

    updated crawler to store json data in a variable

--- draft.py
+++ draft.py
@@ -1,23 +1,28 @@
-# def test(title, location):
-# 	name = 'stackoverflow'
-# 	title = title.replace(" ", "+")
-# 	location = location.replace(" ", "+").replace(",", "%2C")
-# 	start_urls = ['http://www.indeed.com/jobs?q=' + title + '&l=' + location]
-# 	return start_urls
-
-# print test("full stack", "Seattle WA")
+# pip install scrapy
+# --------------To Use---------------
+# import draft
+# print draft.customCrawler()
+# -----------------------------------
 
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
-class DiceSpider(scrapy.Spider):
-	name = 'dice'
+results = []
+
+def customCrawler():
+	process.crawl(diceSpider)
+	process.start()
+	return results
+
+class diceSpider(scrapy.Spider):
+	name = 'back'
 	# start_urls = ['https://www.dice.com/jobs?q=full+stack&l=Seattle%2C+WA']
-	start_urls = ['https://www.dice.com/jobs/q-full+stack+developer-limit-120-jobs.html']
-
+	start_urls = ['https://www.dice.com/jobs/q-"back+end"+"full+stack"+"front+end"-limit-30-jobs.html']
+	
 	def parse(self, response):
 		page = 1
-		while response.css('.serp-result-content h3 a::attr(href)') and page < 2:
-			url = 'https://www.dice.com/jobs/q-full+stack+developer-startPage-'+str(page)+'-limit-120-jobs'
+		while response.css('.serp-result-content h3 a::attr(href)') and page < 8:
+			url = 'https://www.dice.com/jobs/q-"back+end"+"full+stack"+"front+end"-startPage-'+str(page)+'-limit-30-jobs.html'
 			yield scrapy.Request(url, callback=self.parse_page)
 			page += 1
 
@@ -27,18 +32,14 @@ class DiceSpider(scrapy.Spider):
 			yield scrapy.Request(full_url, callback=self.parse_job)
 
 	def parse_job(self, response):
-	    yield {
+		results.append({
 			'title': response.css('.jobTitle::text').extract()[0],
 			'skills': response.css('.iconsiblings::text').extract()[0],
 			'salary': response.css('.mL20::text').extract()[0],
 			'location': response.css('.location::text').extract()[0],
 			'posted': response.css('.posted::text').extract()[0],
-			# 'description': response.css('#jobdescSec').extract()[0],
-			# 'travel': response.css('.iconsiblings').extract()[2],
-			# 'tags': response.css('.question .post-tag::text').extract(),
-			# 'link': response.url,
+		})
 
-
-		}
-# https://www.dice.com/jobs/q-full+stack+developer-startPage-limit-120-jobs.html
-# response.css('.posiCount span::text').extract()[3]
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
